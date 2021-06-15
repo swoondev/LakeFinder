eb346c396d565f19d8c0eb43c2c519c3b3536c78
Swoon <you@example.com>
2021-06-14 23:45:52 -0500

    algorithm added for both single and all county

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -17,16 +17,23 @@ export class HomePageComponent implements OnInit {
   counties: {County: string, Id: number}[] = staticcontent.counties;
   species: {Species: string, Id: string}[] = staticcontent.Species;
 
+  fishtable: any[] = [];
+
   constructor(private apiService: ApiService) { }
 
   private GetLengthCount(survey, species, min, max, show) {
     let count = 0;
-    console.log(survey.lengths[species]);
-    // for (let i = 0; i<survey.lengths[species].fishCount.length; i++){
-    //   if (survey.lengths[species].fishCount[i][0] >= min && survey.lengths[species].fishCount[i][0] <= max){
-    //     count += survey.lengths[species].fishcount[i][1];
-    //   }
-    // }
+    //console.log(survey.lengths[species]);
+    if(survey.lengths[species] != undefined){
+      for (let i = 0; i<survey.lengths[species].fishCount.length; i++){
+        if (survey.lengths[species].fishCount[i][0] >= min && survey.lengths[species].fishCount[i][0] <= max){
+          //console.log(survey.lengths[species].fishCount)
+          count += survey.lengths[species].fishCount[i][1];
+        }
+      }
+    }
+    return count;
+
   }
 
   private revealfishlengthstats(survey){
@@ -49,17 +56,38 @@ export class HomePageComponent implements OnInit {
   }
 
   async searchLakes(countyInput) {
-    console.log(this.countyInput);
-    console.log(this.speciesInput);
+    //console.log(this.countyInput);
+    //console.log(this.speciesInput);
     if(countyInput == 0){
       for(let i=1;i<88;i++){
         await this.apiService.GetLakesByCounty(i).subscribe(lakes => {
-          console.log(lakes);
+          //console.log(lakes);
           if(lakes.results.length != 0){
             lakes.results.forEach(element => {
-              this.lakeArray.push(element);
-              this.apiService.GetLakeData(element.Id).subscribe(survey => {
-                console.log(survey);
+              // this.lakeArray.push(element);
+              // console.log(this.lakeArray);
+              //console.log(element.id)
+              this.apiService.GetLakeData(element.id).subscribe(survey => {
+                if(survey.status == "SUCCESS" && survey.message == "Normal execution."){
+                  let surveyData = survey.result.surveys;
+                  surveyData.forEach(data => {
+                    data.surveyDate = new Date(data.surveyDate);
+                  })
+                  surveyData.sort((a,b) => {
+                    return a.surveyDate-b.surveyDate;
+                  });
+                  surveyData = surveyData.filter(x => x.surveyType == "Standard Survey")
+                  let speciesdata = surveyData[surveyData.length-1].fishCatchSummaries;
+                  speciesdata = speciesdata.filter(fish => fish.species == this.speciesInput);
+                  //console.log("speciesData", speciesdata);
+                  if (speciesdata.length != 0) {
+                    let fishlenarray = this.revealfishlengthstats(surveyData[surveyData.length-1]);
+                    this.fishtable.push({name: element.name, speciesdata: speciesdata[speciesdata.length-1], fishlengths: fishlenarray})
+                    console.log("Lake:", element.name);
+                    console.log(fishlenarray);
+                    console.log(speciesdata[speciesdata.length-1]);
+                  }
+                }
               })
             });
           }
@@ -88,8 +116,11 @@ export class HomePageComponent implements OnInit {
                 speciesdata = speciesdata.filter(fish => fish.species == this.speciesInput);
                 //console.log("speciesData", speciesdata);
                 if (speciesdata.length != 0) {
-                  let fishlenarray = this.revealfishlengthstats(surveyData[surveyData.length-1])
+                  let fishlenarray = this.revealfishlengthstats(surveyData[surveyData.length-1]);
+                  this.fishtable.push({name: lake.name, speciesdata: speciesdata[speciesdata.length-1], fishlengths: fishlenarray})
+                  console.log("Lake:", lake.name);
                   console.log(fishlenarray);
+                  console.log(speciesdata[speciesdata.length-1]);
                 }
               }
             })
